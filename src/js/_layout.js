bc5cbb32ff4ed8c4dcbf4e34a9475dcab0b7b9f3
AVorona <vitalii.parshikov@gmail.com>
2017-12-14 20:35:17 +0200

    debounce added

--- src/js/_layout.js
+++ src/js/_layout.js
@@ -566,14 +566,29 @@ imagesLoaded('.js-masonry', { background: '.plate__item' }, function() {
   msnry.layout();
 });
 
-window.addEventListener('resize', function(e) {
-  // console.log('resize');
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
-  setTimeout(() => {
-    msnry.layout();
-  }, 2000);
+window.addEventListener(
+  'resize',
+  debounce(function(e) {
+    // console.log('resize');
 
-  setTimeout(() => {
-    msnry.layout();
-  }, 2100);
-});
+    setTimeout(() => {
+      msnry.layout();
+    }, 2000);
+  })
+);
